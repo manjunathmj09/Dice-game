c30493228e7cd9225d04c6ea17da4a9232093659
manjunathmj09 <your-email@example.com>
2025-01-31 19:54:19 +0530

    check 1

--- src/comp/RollDice.jsx
+++ src/comp/RollDice.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./RollDice.css";
 const RollDice = ({currentDice,rollDice,reset,setVisible}) => {
-  
+  console.log(currentDice)
   return (
     <div className="roll">
       <div onClick={rollDice}>
-        <img src={`./public/images/dice_${currentDice}.png`} alt="dice" />
+        <img src={`/images/dice_${currentDice}.png`} alt="dice" />
       </div>
       <p>Click on the Dice to Roll</p>
       <button className="btn1" onClick={reset}>Reset Score</button>
--- src/comp/StartGame.jsx
+++ src/comp/StartGame.jsx
@@ -6,7 +6,7 @@ const StartGame = ({toggle}) => {
   return (
     <div className="container">
       <div>
-        <img src=".\public\images\dices 1.png" alt="Dices" />
+        <img src="/images/dices_1.png" alt="Dices" />
       </div>
       <div className="content">
         <div>
