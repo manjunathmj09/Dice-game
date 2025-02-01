7de9d3eb592982084745e6d8ad2766a8fe4474a5
manjunathmj09 <your-email@example.com>
2025-02-01 20:57:58 +0530

    check6

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  path: "./",
+  path: "/dice-game",
   build: {
     outDir: "dist",
     assetsDir: "assets",
